f4f7b504d14ddc85a059dd6b0a5cc7682e98da7e
Терентьев Никита <nitrolacs@MacBook-Air-Terenteva.local>
2023-09-06 19:06:03 +0700

    Add new enum in separate file

--- lab_1/src/main/java/Menu.java
+++ lab_1/src/main/java/Menu.java
@@ -1,11 +1,5 @@
 package src.main.java;
 
-enum MenuChoices {
-	ENGINES_ARE_NOT_ADDED,
-	ENGINE_IS_ADDED,
-	THIS_ITEM_IS_NOT_ON_THE_MENU
-}
-
 public class Menu {
     public static void printMenu() {
         System.out.println("""
