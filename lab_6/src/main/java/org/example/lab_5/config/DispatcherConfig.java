081689a23f783efec6719ea7db1588126bb39bb4
Терентьев Никита <nitrolacs@MacBook-Air-Terenteva.local>
2023-10-21 20:53:57 +0700

    Added security config

--- lab_6/src/main/java/org/example/lab_5/config/DispatcherConfig.java
+++ lab_6/src/main/java/org/example/lab_5/config/DispatcherConfig.java
@@ -18,7 +18,7 @@ public class DispatcherConfig extends AbstractAnnotationConfigDispatcherServletI
      */
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[]{AppConfig.class};
+        return new Class<?>[]{AppConfig.class, SecurityConfig.class};
     }
 
     /**
---
+++ lab_6/src/main/java/org/example/lab_5/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package org.example.lab_5.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                // Доступ только для не зарегистрированных пользователей
+                .antMatchers("/registration").not().fullyAuthenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/bicycles/**").hasAnyRole("ADMIN", "USER")
+                .anyRequest().authenticated()
+                .and()
+                //Настройка для входа в систему
+                .formLogin()
+                .loginPage("/login")
+                //Перенарпавление на главную страницу после успешного входа
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .logoutSuccessUrl("/");
+    }
+}
