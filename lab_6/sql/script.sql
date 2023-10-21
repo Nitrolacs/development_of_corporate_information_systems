1d33bf03f7607fe123c6ce2f8e37dc3a08f0335d
Терентьев Никита <nitrolacs@MacBook-Air-Terenteva.local>
2023-10-21 19:05:30 +0700

    Start developing lab_6!

---
+++ lab_6/sql/script.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE db_for_java;
+\c db_for_java;
+CREATE TABLE Bike(
+    id serial PRIMARY KEY,
+    price double precision NOT NULL,
+    numberOfSpeeds integer NOT NULL,
+    name varchar NOT NULL,
+    type varchar NOT NULL,
+    frameMaterial varchar NOT NULL
+);
---
+++ lab_6/src/main/java/org/example/lab_5/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package org.example.lab_5.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Контроллер домашней страницы приложения
+ */
+@Controller
+public class HomeController {
+
+    /**
+     * Возвращает представление домашней страницы приложения
+     * @return представление
+     */
+    @GetMapping("/")
+    public String showHomePage() {
+        return "bicycles/home";
+    }
+}
