5574748599d4781a7d1b14c3f83c6ea0bfc1ee9d
Terentev Nikita <nitro-2003@mail.ru>
2023-10-28 11:52:30 +0700

    Login is now working!

---
+++ lab_6/src/main/java/org/example/lab_6/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package org.example.lab_6.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends
+        AbstractSecurityWebApplicationInitializer {
+}
